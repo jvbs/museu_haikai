65888e67d61265dce156be5c80c78d61c1733202
Joao Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-06-01 00:29:51 -0300

    correçao pagina home menus

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-4">
+        <div class="col-lg-4 col-xs-6 mb-3">
             <div class="card">
-                <img class="card-img-top"
+                {{-- <img class="card-img-top"
                     src="https://via.placeholder.com/268x130.png"
-                    alt="Card image cap">
+                    alt="Card image cap"> --}}
                 <div class="card-body text-center">
                     <h5 class="card-title">Minhas obras</h5>
                     <p class="card-text">Clique para visualizar as suas obras criadas.</p>
@@ -15,14 +15,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 col-xs-6">
             <div class="card">
-                <img class="card-img-top"
+                {{-- <img class="card-img-top"
                     src="https://via.placeholder.com/268x130.png"
-                    alt="Card image cap">
+                    alt="Card image cap"> --}}
                 <div class="card-body text-center">
                     <h5 class="card-title">Meu perfil</h5>
-                    <p class="card-text">Clique para visualizar o seu perfil.</p>
+                    <p class="card-text">Clique para visualizar informações do seu perfil.</p>
                     <a href="{{ route('admin.perfil.edit', auth()->user()->id) }}" class="btn btn-primary">Meu perfil</a>
                 </div>
             </div>
